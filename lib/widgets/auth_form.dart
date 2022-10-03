d4adff31dc173b977153231cebdf4f85ea0ae2e7
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-10-03 12:56:28 +0600

    remove textinputaction next

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -10,7 +10,6 @@ class AuthForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-    final FocusNode confirmPasswordNode = FocusNode();
 
     return Form(
       key: formKey,
@@ -25,26 +24,18 @@ class AuthForm extends StatelessWidget {
                 prefixIcon: Icons.email_rounded,
               ),
               keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.next,
+              textInputAction: TextInputAction.done,
               autocorrect: false,
             ),
           ),
           const SizedBox(height: 15),
-          PasswordField(
-            hintText: 'Password',
-            textInputAction: TextInputAction.next,
-            requestFocus: confirmPasswordNode,
-          ),
+          const PasswordField(hintText: 'Password'),
           const SizedBox(height: 15),
           if (authMode == AuthMode.register)
             Column(
-              children: [
-                PasswordField(
-                  hintText: 'Confirm Password',
-                  textInputAction: TextInputAction.next,
-                  focusNode: confirmPasswordNode,
-                ),
-                const SizedBox(height: 15),
+              children: const [
+                PasswordField(hintText: 'Confirm Password'),
+                SizedBox(height: 15),
               ],
             ),
           ElevatedButton(
@@ -65,15 +56,9 @@ class PasswordField extends StatefulWidget {
   const PasswordField({
     super.key,
     this.hintText,
-    this.textInputAction,
-    this.focusNode,
-    this.requestFocus,
   });
 
   final String? hintText;
-  final TextInputAction? textInputAction;
-  final FocusNode? focusNode;
-  final FocusNode? requestFocus;
 
   @override
   State<PasswordField> createState() => _PasswordFieldState();
@@ -103,15 +88,9 @@ class _PasswordFieldState extends State<PasswordField> {
           ),
         ),
         obscureText: _obscureText,
-        textInputAction: widget.textInputAction,
+        textInputAction: TextInputAction.done,
         enableSuggestions: false,
         autocorrect: false,
-        focusNode: widget.focusNode,
-        onFieldSubmitted: (_) {
-          if (widget.requestFocus != null) {
-            FocusScope.of(context).requestFocus(widget.requestFocus);
-          }
-        },
       ),
     );
   }
