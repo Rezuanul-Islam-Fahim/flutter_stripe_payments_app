d63a31b31bfc5ebce9e368b1b4e01c4a6648e39d
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-10-08 23:39:02 +0600

    Add log out aler dialog

--- lib/views/home/widgets/custom_drawer.dart
+++ lib/views/home/widgets/custom_drawer.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -22,7 +24,34 @@ class CustomDrawer extends StatelessWidget {
           ListTile(
             title: const Text('Logout'),
             leading: const Icon(Icons.logout),
-            onTap: () {},
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return _buildLogoutConfirm(context);
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLogoutConfirm(BuildContext context) {
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+      child: AlertDialog(
+        actionsPadding: const EdgeInsets.fromLTRB(0, 0, 20, 15),
+        title: const Text('Are you sure you want to logout?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('Cancel'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Logout'),
           ),
         ],
       ),
